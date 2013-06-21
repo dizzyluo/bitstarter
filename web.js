1355809bbbd4921db88a6717f04c2b05605e90b4
root <root@carbon.(none)>
2013-06-22 05:48:27 +0800

    part 1

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
-var express = require('express');
+// var express = require('express');
 
-var app = express.createServer(express.logger());
+// var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+// app.get('/', function(request, response) {
+//   response.send('Hello World!');
+// });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+// var port = process.env.PORT || 5000;
+// app.listen(port, function() {
+//   console.log("Listening on " + port);
+// });
+
+#!/usr/bin/env node
+var fs = require('fs');
+var outfile = "hello.txt";
+var out = "Modify this script to write someting different.\n";
+fs.fs.writeFileSync(outfile, out);
+console.log("Script " + __filename + "\nWrote " + out + " To " + outfile);
