36dbc1e210ca253015641478f67b6a1016d81bb5
root <root@carbon.(none)>
2013-07-15 20:54:01 +0800

    Updated web.js to pull index.html - Working

--- web.js
+++ web.js
@@ -4,11 +4,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 app.get('/', function(req, res) {
 	var buffer = fs.readFileSync('index.html');
-	buffer.toString('utf-8');
-	// res.send("Hello World 2!");
+	// buffer.toString('utf-8');
+	res.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-	console.log("listing to " + port);
+	console.log("Listening to " + port);
 });
