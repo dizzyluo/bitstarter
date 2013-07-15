e57fdd238d8a44446fef361a9406b4df1e95c782
root <root@carbon.(none)>
2013-07-15 20:51:42 +0800

    added pulling of data from index.html

--- web.js
+++ web.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
+var fs = require('fs');
 var express = require('express');
 var app = express.createServer(express.logger());
 app.get('/', function(req, res) {
-	res.send("Hello World 2!");
+	var buffer = fs.readFileSync('index.html');
+	buffer.toString('utf-8');
+	// res.send("Hello World 2!");
 });
 
 var port = process.env.PORT || 5000;
