4a693deb3a5734537aa23a556fbae0edf58bbb04
root <root@carbon.(none)>
2013-07-15 20:24:04 +0800

    Bug fix web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express.createServer(express.logger());
 app.get('/', function(req, res) {
-	response.send("Hello World!");
+	res.send("Hello World!");
 });
 
 var port = process.env.PORT || 5000;
